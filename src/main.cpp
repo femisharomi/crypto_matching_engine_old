18b5b2d04253f8bcc2305a755916cf74d31fa795
Femi Sharomi <Mrfemi1998@gmail.com>
2026-01-24 01:51:16 +0000

    Git Repo created and linked

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Crypto Matching Engine starting...\n";
+    return 0;
+}
---
+++ tests/example_test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(Example, SimpleCheck) {
+    EXPECT_EQ(1 + 1, 2);
+}
